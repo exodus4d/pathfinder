052149fb122855a799245c83e1bf3aa657bfba26
Exodus4D <exodus4d@gmail.com>
2015-08-27 19:08:12 +0200

    og-tag image location

--- index.php
+++ index.php
@@ -20,6 +20,9 @@ $f3->set('BASE', \Controller\Controller::getEnvironmentData('BASE'));
 // set debug  level (stacktrace)
 $f3->set('DEBUG', \Controller\Controller::getEnvironmentData('DEBUG'));
 
+// set debug  level (stacktrace)
+$f3->set('URL', \Controller\Controller::getEnvironmentData('URL'));
+
 // initiate cron-jobs
 Cron::instance();
 
