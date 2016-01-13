0fa721081083bd0102360f5f6d6da3a162f350c7
Exodus4D <exodus4d@gmail.com>
2016-01-13 19:39:42 +0100

    added "lazy loading" for youtube video improved "PageSpeed Insights" Ranking

--- js/app/landingpage.js
+++ js/app/landingpage.js
@@ -402,6 +402,35 @@ define([
         });
     };
 
+    var initYoutube = function(){
+
+        $(".youtube").each(function() {
+            // Based on the YouTube ID, we can easily find the thumbnail image
+            $(this).css('background-image', 'url(https://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
+
+            // Overlay the Play icon to make it look like a video player
+            $(this).append($('<div/>', {'class': 'play'}));
+
+            $(document).delegate('#'+this.id, 'click', function() {
+                // Create an iFrame with autoplay set to true
+                var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+                if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+
+                // The height and width of the iFrame should be the same as parent
+                var iframe = $('<iframe/>', {
+                    frameborder: '0',
+                    src: iframe_url,
+                    width: $(this).width(),
+                    height: $(this).height(),
+                    class: 'pricing-big'
+                });
+
+                // Replace the YouTube thumbnail with YouTube HTML5 Player
+                $(this).replaceWith(iframe);
+            });
+        });
+    };
+
 
     /**
      * init scrollspy for navigation bar
@@ -510,6 +539,9 @@ define([
 
         // init gallery
         initGallery();
+
+        // init youtube videos
+        initYoutube();
 
         // show logo -> hide animation in IGB
         if( !CCP.isInGameBrowser() ){
