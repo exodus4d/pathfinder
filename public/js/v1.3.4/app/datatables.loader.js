f2fc821181220580fc514328681b6e437afe26d1
Mark Friedrich <exodus4d@users.noreply.github.com>
2018-02-24 23:37:08 +0100

    - v1.3.4 preparation

---
+++ public/js/v1.3.4/app/datatables.loader.js
@@ -0,0 +1,11 @@
+define([
+    'datatables.net',
+    'datatables.net-buttons',
+    'datatables.net-buttons-html',
+    'datatables.net-responsive',
+    'datatables.net-select'
+], (a, b) => {
+    'use strict';
+
+    // all Datatables stuff is available...
+});
