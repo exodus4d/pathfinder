00f03f1ec16e6474831cdadbf23ecfdacca12406
Harry S <harry@sysop.ooo>
2019-10-27 16:37:48 -0600

    Set destination experiments (#1)
    * attempt to make destinations work
    
    * ez debug logging
    
    * js debugging
    
    * remove debug bits
    
    * use camel case key name systemId for setDestination

--- js/app/map/map.js
+++ js/app/map/map.js
@@ -641,10 +641,16 @@ define([
                 $.fn.showDeleteSystemDialog(map, selectedSystems);
                 break;
             case 'set_destination':
+                systemData = system.getSystemData();
+                Util.setDestination('set_destination', 'system', systemData);
+                break;
             case 'add_first_waypoint':
+                systemData = system.getSystemData();
+                Util.setDestination('add_first_waypoint', 'system', systemData);
+                break;
             case 'add_last_waypoint':
                 systemData = system.getSystemData();
-                Util.setDestination(systemData, action);
+                Util.setDestination('add_last_waypoint', 'system', systemData);
                 break;
         }
     };
