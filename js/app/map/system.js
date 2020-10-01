6b851b12374ccd93c7fb45794404d2b84d348d0b
soneill <sam@adcurve.com>
2020-10-02 12:07:51 +1300

    fixes static for hs connections

--- js/app/map/system.js
+++ js/app/map/system.js
@@ -804,7 +804,7 @@ define([
                         Util.getSecurityClassForSystem(staticData.security),
                         Util.config.popoverTriggerClass
                     ].join(' '),
-                    textContent: MapUtil.getSystemSecurityForDisplay(staticData.security.toLowerCase())
+                    textContent: MapUtil.getSystemSecurityForDisplay(staticData.security).toLowerCase()
                 });
                 staticEl.dataset.name = staticData.name;
                 headInfoRight.push(staticEl);
