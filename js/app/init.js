58c608582adc624664bf43fab26e50eb77eb6c02
soneill <sam@adcurve.com>
2021-07-12 11:31:54 +1200

    Updates wh mass limits as per QOL patch from 23/03/21

--- js/app/init.js
+++ js/app/init.js
@@ -479,28 +479,28 @@ define([], () => {
         },
         wormholeSizes: {
             wh_jump_mass_xl: {
-                jumpMassMin: 1000000000,
+                jumpMassMin: 1350000000,
                 type: 'wh_jump_mass_xl',
                 class: 'pf-jump-mass-xl',
                 label: 'XL',
                 text: 'capital ships'
             },
             wh_jump_mass_l: {
-                jumpMassMin: 300000000,
+                jumpMassMin: 375000000,
                 type: 'wh_jump_mass_l',
                 class: 'pf-jump-mass-l',
                 label: 'L',
                 text: 'larger ships'
             },
             wh_jump_mass_m: {
-                jumpMassMin: 20000000,
+                jumpMassMin: 62000000,
                 type: 'wh_jump_mass_m',
                 class: 'pf-jump-mass-m',
                 label: 'M',
                 text: 'medium ships'
             },
             wh_jump_mass_s: {
-                jumpMassMin: 1000,
+                jumpMassMin: 5000,
                 type: 'wh_jump_mass_s',
                 class: 'pf-jump-mass-s',
                 label: 'S',
