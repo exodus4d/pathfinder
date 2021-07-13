2fb88a1b1295777b20aaab4f26b5476f96bd0111
soneill <sam@adcurve.com>
2021-07-13 14:48:53 +1200

    updates source repo in github API

--- app/Controller/Api/GitHub.php
+++ app/Controller/Api/GitHub.php
@@ -35,7 +35,7 @@ class GitHub extends Controller\Controller {
         $return->version->delta = null;
         $return->version->dev = false;
 
-        $releases = $f3->gitHubClient()->send('getProjectReleases', 'exodus4d/pathfinder', $releaseCount);
+        $releases = $f3->gitHubClient()->send('getProjectReleases', 'goryn-clade/pathfinder', $releaseCount);
 
         foreach($releases as $key => &$release){
             // check version ------------------------------------------------------------------------------------------
